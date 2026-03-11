d1cf89a07997cdcf9ebbfd037a1344d4f220d495
Raghad-Moqady <raghadmoqady@gmail.com>
2026-03-11 23:42:31 +0200

    Storage|use PostgreSQL Database ,Drizzle ORM,Drizzle Queries

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,11 @@
+process.loadEnvFile();
+function envOrThrow(key) {
+    const value = process.env[key];
+    if (!value)
+        throw new Error(`Missing environment variable: ${key}`);
+    return value;
+}
 export const config = {
+    dbURL: envOrThrow("DB_URL"),
     fileserverHits: 0
 };
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,17 @@
+process.loadEnvFile();
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) throw new Error(`Missing environment variable: ${key}`);
+  return value;
+}
+ 
 type APIConfig = {
+  dbURL: string,
   fileserverHits: number;
 };
+
 export const config: APIConfig = {
+  dbURL: envOrThrow("DB_URL"),
   fileserverHits: 0
 };
