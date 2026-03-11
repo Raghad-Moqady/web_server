dbf0c754ba946d4a41d5e8539a17db6350866db2
Raghad-Moqady <raghadmoqady@gmail.com>
2026-03-11 03:22:22 +0200

    Add a new endpoint(Post) & manual parsing

--- dist/index.js
+++ dist/index.js
@@ -22,11 +22,43 @@ app.get("/admin/metrics", (req, res) => {
 </html>
 `);
 });
-app.get("/admin/reset", (req, res) => {
+app.post("/admin/reset", (req, res) => {
     config.fileserverHits = 0;
     res
         .set("Content-Type", "text/plain; charset=utf-8")
         .send("Hits reset to 0");
+});
+app.post("/api/validate_chirp", (req, res) => {
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+    req.on("end", () => {
+        try {
+            const parsed = JSON.parse(body);
+            const chirp = parsed.body;
+            if (!chirp) {
+                res.status(400).json({
+                    error: "Something went wrong"
+                });
+                return;
+            }
+            if (chirp.length > 140) {
+                res.status(400).json({
+                    error: "Chirp is too long"
+                });
+                return;
+            }
+            res.status(200).json({
+                valid: true
+            });
+        }
+        catch (err) {
+            res.status(400).json({
+                error: "Invalid JSON"
+            });
+        }
+    });
 });
 app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
--- src/index.ts
+++ src/index.ts
@@ -27,11 +27,48 @@ app.get("/admin/metrics",(req:Request,res:Response)=>{
 </html>
 `);
 })
-app.get("/admin/reset", (req: Request, res: Response) => {
+app.post("/admin/reset", (req: Request, res: Response) => {
   config.fileserverHits = 0; 
   res
     .set("Content-Type", "text/plain; charset=utf-8")
     .send("Hits reset to 0");
+});
+app.post("/api/validate_chirp", (req: Request, res: Response) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+  req.on("end", () => {
+    try {
+      const parsed = JSON.parse(body);
+
+      const chirp = parsed.body;
+
+      if (!chirp) {
+        res.status(400).json({
+          error: "Something went wrong"
+        });
+        return;
+      }
+
+      if (chirp.length > 140) {
+        res.status(400).json({
+          error: "Chirp is too long"
+        });
+        return;
+      }
+
+      res.status(200).json({
+        valid: true
+      });
+
+    } catch (err) {
+      res.status(400).json({
+        error: "Invalid JSON"
+      });
+    }
+  });
+
 });
 app.use("/app",middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
